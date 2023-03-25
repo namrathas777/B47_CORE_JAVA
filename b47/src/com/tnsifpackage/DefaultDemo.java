ecddbbbbd130a75ae99c356418bf3f24eba70034
namra <namra@namratha>
2023-03-25 20:16:41 +0530

    Default constructor

---
+++ b47/src/com/tnsifpackage/DefaultDemo.java
@@ -0,0 +1,11 @@
+package com.tnsifpackage;
+
+public class DefaultDemo {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Defaultcons ob=new Defaultcons();//invoking default constructor
+		System.out.println(ob);
+	}
+}
---
+++ b47/src/com/tnsifpackage/Defaultcons.java
@@ -0,0 +1,9 @@
+package com.tnsifpackage;
+
+public class Defaultcons {
+
+	Defaultcons(){
+		System.out.println("default cons");
+	}
+
+}
