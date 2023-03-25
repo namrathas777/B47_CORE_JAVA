6ee8a91173604e625841fa9e95715d4f040c70aa
namra <namra@namratha>
2023-03-25 20:08:31 +0530

    getters and setters

---
+++ b47/src/com/tnsifpackage/CustomerDemo.java
@@ -0,0 +1,21 @@
+package com.tnsifpackage;
+import java.util.Scanner;
+public class CustomerDemo{
+	public static void main(String[] arga) {
+		Scanner Sc =new Scanner(System.in);
+		String name,city;
+		int id;
+		System.out.println("Enter Customer name");
+		name= Sc.nextLine();
+		System.out.println("Enter Customer id");
+		id=Sc.nextInt();
+		System.out.println("Enter Customer city");
+		city=Sc.next();
+		System.out.println(" ");
+		Customer ob =new Customer(); 
+		ob.setCustomerId(id);
+		ob.setCustomerName(name);
+		ob.setCustomerCity(city);
+	}
+
+}
